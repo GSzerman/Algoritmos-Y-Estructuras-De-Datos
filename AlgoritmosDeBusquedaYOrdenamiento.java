919ed0969fcfdc56deb860dcd430e7279c1cf2e0
Szerman <gabriel.szerman@accenture.com>
2023-11-09 00:04:34 -0300

    primer commit de prueba

--- AlgoritmosDeBusquedaYOrdenamiento.java
+++ AlgoritmosDeBusquedaYOrdenamiento.java
@@ -5,12 +5,23 @@ public class AlgoritmosDeBusquedaYOrdenamiento {
         System.out.println("Probando");
         System.out.println(sumar(5,10));
 
+
+
+       int[] vectorOrdenadoSinRepetidos = {-2,-1,3,7,9,15};
+
+
+       
+    for(int i=0; i<vectorOrdenadoSinRepetidos.length; i++){
+        System.out.println(vectorOrdenadoSinRepetidos[i]);
+    }
+
     }
 
 
     private static int sumar(int x , int y){
         return(x+y);
     }
+
 
     
 }
