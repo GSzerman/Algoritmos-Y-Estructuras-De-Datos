a09ddcf854392cb4a059d492aba648ba523654e0
Gabriel <gabriel.szerman@gmail.com>
2023-11-08 23:05:33 -0300

    Primer commit de prueba

---
+++ AlgoritmosDeBusquedaYOrdenamiento.java
@@ -0,0 +1,16 @@
+public class AlgoritmosDeBusquedaYOrdenamiento {
+
+    public static void main(String[] args){
+
+        System.out.println("Probando");
+        System.out.println(sumar(5,10));
+
+    }
+
+
+    private static int sumar(int x , int y){
+        return(x+y);
+    }
+
+    
+}
